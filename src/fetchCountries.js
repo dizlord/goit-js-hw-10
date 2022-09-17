4f098def6e6d2dea6d5c2c24c28d84c115f6164c
Illia Kazmin <kazmin1c@gmail.com>
2022-09-17 20:00:07 +0200

    add markup for one country

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,10 +2,9 @@ const filterOptions = '?fields=name,capital,population,flags,languages';
 
 function fetchCountries(name) {
   const url = `https://restcountries.com/v3.1/name/${name}`;
-  // console.log(url + filterOptions);
+
   return fetch(url + filterOptions)
     .then(response => {
-      // console.log(response);
       if (!response.ok) {
         throw new Error(response.status);
       }
--- src/index.js
+++ src/index.js
@@ -10,35 +10,29 @@ const refs = {
   countryListUl: document.querySelector('.country-list'),
   countryInfoDiv: document.querySelector('.country-info'),
 };
-// debounce(func, 300);
-// Notiflix.Notify.success('Sol lucet omnibus');
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-// Notiflix.Notify.warning('Memento te hominem esse');
-// Notiflix.Notify.info('Cogito ergo sum');
 
 refs.searchInput.addEventListener('input', debounce(onInput, 300));
 
 function onInput() {
-  // evt.preventDefault();
-
   const searchName = String(refs.searchInput.value);
-  // console.log(searchName);
 
   if (searchName.trim()) {
     fetchCountries(searchName)
       .then(countries => {
-        console.log(countries);
         if (countries.length > 10) {
           Notiflix.Notify.info(
             'Too many matches found. Please enter a more specific name.'
           );
         } else if (countries.length >= 2 && countries.length < 10) {
           makeMarkupCountires(countries);
+        } else {
+          makeMarkupCountry(countries);
         }
       })
-      .catch(error =>
-        Notiflix.Notify.failure('Oops, there is no country with that name')
-      );
+      .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        clearCountryContainer();
+      });
   } else {
     clearCountryContainer();
   }
@@ -50,14 +44,13 @@ function clearCountryContainer() {
 }
 
 function makeMarkupCountires(countries = []) {
+  clearCountryContainer();
   const markupCounries = countries
     .map(country => {
-      // console.log(country);
       return `
         <li class="country-list__item">
-          <svg class="country-list__icon" width="40px" height="30px">
-            <use href="${country.flags.svg}"></use>
-          </svg>
+          <img class="country-list__icon" width="40px" height="30px"
+           src="${country.flags.svg}" alt="${country.name.official}">
           <span class="country-list__countryName">
             ${country.name.official}
           </span>
@@ -65,6 +58,39 @@ function makeMarkupCountires(countries = []) {
       `;
     })
     .join('');
-  console.log(markupCounries);
   refs.countryListUl.innerHTML = markupCounries;
+}
+
+function makeMarkupCountry(countries = []) {
+  clearCountryContainer();
+  const markupCounries = countries
+    .map(country => {
+      return `
+        <div class="country-info__item">
+          <img class="country-list__icon" src="${country.flags.svg}" alt="${
+        country.name.official
+      }" width="40px" height="30px" />
+          <h2 class="country-info__title">${country.name.official}</h2>
+        </div>
+        <div class="article-wrapper">
+          <p class="country-info__article">
+            Capital:
+          </p><span class="country-info__span">${country.capital}</span>
+        </div>
+        <div class="article-wrapper">
+          <p class="country-info__article">
+            Population:
+          </p><span class="country-info__span">${country.population}</span>
+        </div>
+        <div class="article-wrapper">
+          <p class="country-info__article">
+            Languahes:
+          </p><span class="country-info__span">${Object.values(
+            country.languages
+          ).join(',')}</span>
+        </div>
+      `;
+    })
+    .join('');
+  refs.countryInfoDiv.innerHTML = markupCounries;
 }
